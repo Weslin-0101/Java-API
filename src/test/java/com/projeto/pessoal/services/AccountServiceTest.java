59fb6ada5b33a822dd9f3ca848252bcfed033f08
Wesley Lira <wesleeylirac12@gmail.com>
2023-07-23 11:20:45 -0300

    test: implemented findAll function

--- src/test/java/com/projeto/pessoal/services/AccountServiceTest.java
+++ src/test/java/com/projeto/pessoal/services/AccountServiceTest.java
@@ -7,19 +7,33 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AccountServiceTest {
 
-    private Account AccountMock(int i) {
+    private Account AccountMock() {
         Account account = new Account();
         account.setId((1L));
-        account.setName("weslin " + i);
-        account.setEmail("weslin@gmail.com " + i);
+        account.setName("weslin");
+        account.setEmail("weslin@gmail.com");
         account.setPassword("123456");
+
+        return account;
+    }
+
+    private Account[] AccountMockArray(int i) {
+        Account[] account = new Account[i];
+
+        for (int j = 1; j < i; j++) {
+            account[j] = new Account();
+            account[j].setId((1L));
+            account[j].setName("weslin " + j);
+            account[j].setEmail("weslin@gmail.com " + j);
+            account[j].setPassword("123456 " + j);
+        }
 
         return account;
     }
 
     @Test
     void findById() throws Exception {
-        Account ac = AccountMock(0);
+        Account ac = AccountMock();
 
         Long id = new AccountService().findById(ac.getId()).getId();
 
@@ -28,10 +42,19 @@ class AccountServiceTest {
 
     @Test
     void findByName() throws Exception {
-        Account ac = AccountMock(0);
+        Account ac = AccountMock();
 
         String name = new AccountService().findByName(ac.getName()).getName();
 
         assertEquals(ac.getName(), name);
+    }
+
+    @Test
+    void findAll() throws Exception {
+        Account[] ac = AccountMockArray(5);
+
+        String name = new AccountService().findAll().getName();
+
+        assertEquals(ac[2].getName(), name);
     }
 }
