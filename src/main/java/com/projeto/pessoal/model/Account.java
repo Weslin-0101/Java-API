193760c8af1ee6222301d48bc072d2af53b5930b
Wesley Lira <wesleeylirac12@gmail.com>
2023-07-30 12:24:56 -0300

    feat: added AccountModel as Table entity

--- src/main/java/com/projeto/pessoal/model/Account.java
+++ src/main/java/com/projeto/pessoal/model/Account.java
@@ -14,7 +14,7 @@ public class Account implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, length = 100)
