e0e885645ca86c35aed71ab79175daef70198109
Wesley Lira <wesleeylirac12@gmail.com>
2023-08-01 19:56:26 -0300

    chore: add springboot security dependency

---
+++ src/main/java/com/projeto/pessoal/exceptions/InvalidJwtAuthenticationException.java
@@ -0,0 +1,15 @@
+package com.projeto.pessoal.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
+public class InvalidJwtAuthenticationException extends AuthenticationException {
+
+    private static final Long serialVersionUID = 1L;
+
+    public InvalidJwtAuthenticationException(String msg) {
+        super(msg);
+    }
+}
---
+++ src/main/java/com/projeto/pessoal/securityJwt/JwtTokenProvider.java
@@ -0,0 +1,75 @@
+package com.projeto.pessoal.securityJwt;
+
+import com.projeto.pessoal.exceptions.InvalidJwtAuthenticationException;
+import io.jsonwebtoken.*;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import java.util.Base64;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class JwtTokenProvider {
+    private String secretKey = "secret";
+    private Long validityInMilliseconds = 3600000L; // 1h
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    public String createAccessToken(String email, List<String> roles) {
+        Claims claims = Jwts.claims().setSubject(email);
+        claims.put("roles", roles);
+
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    public Authentication getAuthentication(String token) {
+        UserDetails userDetails = this.userDetailsService.loadUserByUsername(getUserEmail(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    private String getUserEmail(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer "))
+            return bearerToken.substring("Bearer ".length());
+
+        return null;
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            if (claims.getBody().getExpiration().before(new Date())) {
+                return false;
+            }
+
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            throw new InvalidJwtAuthenticationException("Expired or invalid JWT token");
+        }
+    }
+}
