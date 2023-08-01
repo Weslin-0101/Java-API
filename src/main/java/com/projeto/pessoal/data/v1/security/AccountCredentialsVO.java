4fb0f5d6e351acd1d701e67b2b91aa6ed613efaa
Wesley Lira <wesleeylirac12@gmail.com>
2023-08-01 12:34:13 -0300

    feat: added AccountCredentialsVO data

---
+++ src/main/java/com/projeto/pessoal/data/v1/security/AccountCredentialsVO.java
@@ -0,0 +1,45 @@
+package com.projeto.pessoal.data.v1.security;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class AccountCredentialsVO implements Serializable {
+    private static final Long serialVersionUID = 1L;
+
+    private String email;
+    private String password;
+
+    public AccountCredentialsVO(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AccountCredentialsVO that = (AccountCredentialsVO) o;
+        return Objects.equals(email, that.email) && Objects.equals(password, that.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email, password);
+    }
+}
