6f78ee6e8f4381beca7102ef6ad9091705da951e
Wesley Lira <wesleeylirac12@gmail.com>
2023-08-01 23:01:59 -0300

    chore: add UserPermission and AlterTablePerson Database

---
+++ src/main/resources/db/migration/V5_Create_Table_User_Permission.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS public.user_permission (
+    id_user BIGSERIAL NOT NULL,
+    id_permission BIGSERIAL NOT NULL,
+    CONSTRAINT pk_user_permission PRIMARY KEY (id_user),
+    CONSTRAINT uk_user_permission UNIQUE (id_permission),
+    CONSTRAINT fk_user_permission FOREIGN KEY (id_user) REFERENCES public.users (id),
+    CONSTRAINT fk_user_permission_permission FOREIGN KEY (id_permission) REFERENCES public.permission (id)
+)
---
+++ src/main/resources/db/migration/V6_Alter_Table_Person.sql
@@ -0,0 +1,1 @@
+ALTER TABLE public.account ADD COLUMN "enabled" boolean NOT NULL DEFAULT true;
